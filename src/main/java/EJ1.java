d98e135f3818509bda33805365aabd273c425c2e
adrianarmm <areyemor@myuax.com>
2024-04-14 13:57:05 +0200

    5

--- src/main/java/EJ1.java
+++ src/main/java/EJ1.java
@@ -15,4 +15,11 @@ public class EJ1 <T, U> {
     public U getSegundoDato() {
         return segundoDato;
     }
+
+    public static void main(String[] args) {
+        EJ1<Integer, String> pareja = new EJ1<>(1, "hola");
+        System.out.println(pareja.getPrimerDato());
+        System.out.println(pareja.getSegundoDato());
+    }
 }
+
