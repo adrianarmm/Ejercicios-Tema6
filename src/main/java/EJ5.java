72575808d245dbe68e7f0b5ecc88b08df257d485
adrianarmm <areyemor@myuax.com>
2024-04-14 14:14:56 +0200

    20

--- src/main/java/EJ5.java
+++ src/main/java/EJ5.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
-
 public class EJ5 {
+
+    public static void main(String[] args) {
+        ArrayList<String> lista = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Añadir cadena");
+            System.out.println("2. Eliminar cadena");
+            System.out.println("3. Salir");
+            System.out.print("Elige una opción: ");
+            int opcion = scanner.nextInt();
 }
+
