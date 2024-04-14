7cdf5dde245a102df2ee2b6bbc5d814bb5ecef14
adrianarmm <areyemor@myuax.com>
2024-04-14 13:56:27 +0200

    3

--- src/main/java/EJ1.java
+++ src/main/java/EJ1.java
@@ -3,4 +3,8 @@ public class EJ1 <T, U> {
     private T primerDato;
     private U segundoDato;
 
+    public EJ1(T primerDato, U segundoDato) {
+        this.primerDato = primerDato;
+        this.segundoDato = segundoDato;
+    }
 }
