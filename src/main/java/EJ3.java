8a078c6513bfde050a860085256acb6e2251a5d4
adrianarmm <areyemor@myuax.com>
2024-04-14 14:07:02 +0200

    15

--- src/main/java/EJ3.java
+++ src/main/java/EJ3.java
@@ -1,24 +1,25 @@
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-
 public class EJ3 {
     public static void main(String[] args) {
 
-        List<EJ3<Integer, Integer>> randomParejasList1 = new ArrayList<>();
+        List<AbstractMap.SimpleEntry<Integer, Integer>> randomParejasList1 = new ArrayList<>();
         fillListWithRandomParejas(randomParejasList1);
         System.out.println("List 1 (ArrayList): " + randomParejasList1);
 
-        List<EJ3<Integer, Integer>> randomParejasList2 = new LinkedList<>();
+        List<AbstractMap.SimpleEntry<Integer, Integer>> randomParejasList2 = new LinkedList<>();
         fillListWithRandomParejas(randomParejasList2);
         System.out.println("List 2 (LinkedList): " + randomParejasList2);
     }
-    private static void fillListWithRandomParejas(List<EJ3<Integer, Integer>> list) {
+
+    private static void fillListWithRandomParejas(List<AbstractMap.SimpleEntry<Integer, Integer>> list) {
         Random random = new Random();
         for (int i = 0; i < 100; i++) {
-            list.add(new EJ3<>(random.nextInt(1000), random.nextInt(1000)));
+            list.add(new AbstractMap.SimpleEntry<>(random.nextInt(1000), random.nextInt(1000)));
         }
     }
 }
