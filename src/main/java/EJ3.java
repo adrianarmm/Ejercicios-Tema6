526418d8e20d507fedeca29a62fa47fcfb3c0cd3
adrianarmm <areyemor@myuax.com>
2024-04-14 14:04:44 +0200

    11

--- src/main/java/EJ3.java
+++ src/main/java/EJ3.java
@@ -5,4 +5,9 @@ import java.util.Random;
 
 
 public class EJ3 {
+    public static void main(String[] args) {
+
+        List<EJ3<Integer, Integer>> randomParejasList1 = new ArrayList<>();
+        fillListWithRandomParejas(randomParejasList1);
+        System.out.println("List 1 (ArrayList): " + randomParejasList1);
 }
