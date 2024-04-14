a32db8a5347ba39da259adcce3f58f954fa1e776
adrianarmm <areyemor@myuax.com>
2024-04-14 14:00:13 +0200

    7

--- src/main/java/EJ2.java
+++ src/main/java/EJ2.java
@@ -4,4 +4,10 @@ import java.util.List;
 import java.util.Random;
 
 public class EJ2 {
-}
+
+    public static void main(String[] args) {
+        // Create a list using ArrayList
+        List<Integer> randomIntegersList1 = new ArrayList<>();
+        fillListWithRandomIntegers(randomIntegersList1);
+        System.out.println("List 1 (ArrayList): " + randomIntegersList1);
+
