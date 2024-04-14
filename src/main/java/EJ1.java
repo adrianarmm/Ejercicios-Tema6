deae344298ca4df857de8413bf37e6bd17b90cbb
adrianarmm <areyemor@myuax.com>
2024-04-14 13:56:41 +0200

    4

--- src/main/java/EJ1.java
+++ src/main/java/EJ1.java
@@ -7,4 +7,12 @@ public class EJ1 <T, U> {
         this.primerDato = primerDato;
         this.segundoDato = segundoDato;
     }
+
+    public T getPrimerDato() {
+        return primerDato;
+    }
+
+    public U getSegundoDato() {
+        return segundoDato;
+    }
 }
